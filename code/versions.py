3fadc93361c27ceb2c7f2738d0c556654228f039
Lucas Ceschini <ceschini@protonmail.com>
2021-01-04 09:26:48 -0300

    commit inicial

---
+++ code/versions.py
@@ -0,0 +1,19 @@
+# check the versions of key python libraries
+# scipy
+import sklearn
+import statsmodels
+import pandas
+import matplotlib
+import numpy
+import scipy
+print('scipy: %s' % scipy.__version__)
+# numpy
+print('numpy: %s' % numpy.__version__)
+# matplotlib
+print('matplotlib: %s' % matplotlib.__version__)
+# pandas
+print('pandas: %s' % pandas.__version__)
+# statsmodels
+print('statsmodels: %s' % statsmodels.__version__)
+# scikit-learn
+print('sklearn: %s' % sklearn.__version__)
